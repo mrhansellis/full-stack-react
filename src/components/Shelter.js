3e25226a5420e05df165c82cc617e04c50810472
Hans Ellis <bullhorn@gmail.com>
2023-04-24 21:01:26 -0700

    wip on useEffect
    Co-authored-by: Jonathan Liera <jonliera1@gmail.com>
    Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

--- src/components/Shelter.js
+++ src/components/Shelter.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function Shelter() {
-  return (
-    <>
-      <h1>Shelter Homepage</h1>
-    </>
-  )
-}
+  // const section
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [allDinos, setAllDinos] = useState([]);
 
+  // useEffect
+
+  useEffect(() =>{
+    fetch(`http://localhost:5123/api/v2/residents`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status}: ${response.statusText}`);
+      } else {
+        return response.json()
+      }
+    })
+    .then((jsonifiedResponse) => {
+      setAllDinos(jsonifiedResponse.results)
+      setIsLoaded(true)
+    })
+    .catch((error) => {
+      setError(error.message)
+      setIsLoaded(true)
+    });
+  },[])
+  console.log(error);
+  console.log(allDinos)
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  } else if (!isLoaded) {
+    return <h1>...loading...</h1>;
+  } else {
+    return (
+      <>
+        <h1>Hi THERE</h1>
+      </>
+    )
+  }
+  
+  
+}
